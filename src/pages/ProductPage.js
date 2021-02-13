751cd648c46b8ead2f642d223565d3bc725c96cc
memellaguno <memellaguno@gmail.com>
2021-02-13 10:52:45 -0600

    Starting Product Page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -18,10 +18,37 @@ import {
   Container,
 } from '@chakra-ui/react';
 
+function ProductInfo() {
+  return (
+    <Container maxWidth="1200px">
+      <Flex h={500} bg="red">
+        <Flex bg="blue" overflow="hidden" flex="2" overflow="hidden">
+          {/* <Square
+            backgroundImage={'url(' + plant + ')'}
+            w="100%"
+            h="400px"
+            bgPosition="center"
+            bgRepeat="no-repeat"
+            bgPosition="center"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+          /> */}
+          <Image src={plant} minWidth="100%" fit="cover" />
+        </Flex>
+
+        <Flex bg="yellow" flex="1">
+          <Text>Two</Text>
+        </Flex>
+      </Flex>
+    </Container>
+  );
+}
+
 function ProductPage() {
   return (
     <div>
       <Header />
+      <ProductInfo />
       <Footer />
     </div>
   );
